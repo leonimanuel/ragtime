ff1e87e8c49bd304093529cf9d9845617bec85d2
Leon Malisov <leonmalisov@gmail.com>
2025-02-19 13:26:50 -0500

    temporarily remove prompts and tools

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -3,7 +3,7 @@ import { openai } from '@ai-sdk/openai';
 import { streamText, tool } from 'ai';
 import { z } from 'zod';
 import { findRelevantContent } from '@/lib/ai/embedding';
-
+import { systemPrompts } from './system-prompts';
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
@@ -11,28 +11,37 @@ export async function POST(req: Request) {
 
   const result = streamText({
     model: openai('gpt-4o'),
-    system: `You are a helpful assistant. Check your knowledge base before answering any questions.
-    Only respond to questions using information from tool calls.
-    if no relevant information is found in the tool calls, respond, "Sorry, I don't know."`,
-    tools: {
-      addResource: tool({
-        description: `add a resource to your knowledge base.
-          If the user provides a random piece of knowledge unprompted, use this tool without asking for confirmation.`,
-        parameters: z.object({
-          content: z
-            .string()
-            .describe('the content or resource to add to the knowledge base'),
-        }),
-        execute: async ({ content }) => createResource({ content }),
-      }),
-      getInformation: tool({
-        description: `get information from your knowledge base to answer questions.`,
-        parameters: z.object({
-          question: z.string().describe('the users question'),
-        }),
-        execute: async ({ question }) => findRelevantContent(question),
-      })
-    },    
+    // system: 
+    // `
+    //   You are an AI chatbot that helps developers design system architectures using the C4 model 
+    //   and recommends the most suitable tech stack based on their input. 
+    //   Your responses must be structured, concise, and strictly follow a predefined question flow.
+
+    //   Ask one question from the list of CORE QUESTIONSat a time, in a strict sequence (no skipping, backtracking, or reordering).
+    //   If the user's response is ambiguous or incomplete, ask for clarification before proceeding.
+
+    //   The CORE QUESTIONS are:
+    //   ${systemPrompts.coreQuestions.map(q => q.question).join('\n')}
+    // `,
+    // tools: {
+    //   addResource: tool({
+    //     description: `add a resource to your knowledge base.
+    //       If the user provides a random piece of knowledge unprompted, use this tool without asking for confirmation.`,
+    //     parameters: z.object({
+    //       content: z
+    //         .string()
+    //         .describe('the content or resource to add to the knowledge base'),
+    //     }),
+    //     execute: async ({ content }) => createResource({ content }),
+    //   }),
+    //   getInformation: tool({
+    //     description: `get information from your knowledge base to answer questions.`,
+    //     parameters: z.object({
+    //       question: z.string().describe('the users question'),
+    //     }),
+    //     execute: async ({ question }) => findRelevantContent(question),
+    //   })
+    // },    
     messages,
   });
 
