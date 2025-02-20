2463d5a98a11e7d9f8f0c0c84a85dfce71838094
Leon Malisov <leonmalisov@gmail.com>
2025-02-19 22:33:54 -0500

    update call configuration

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -3,9 +3,6 @@ import { openai } from '@ai-sdk/openai';
 import { streamText, tool } from 'ai';
 import { z } from 'zod';
 import { findRelevantContent } from '@/lib/ai/embedding';
-import { systemPrompts } from './system-prompts';
-import { tools } from '@/lib/ai/tools';
-
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
@@ -13,27 +10,31 @@ export async function POST(req: Request) {
 
   const result = streamText({
     model: openai('gpt-4o'),
-    // system: systemPrompts.c4Model,
-    tools: {
-      displayWeather: tools.displayWeather,
-      // addResource: tool({
-      //   description: `add a resource to your knowledge base.
-      //     If the user provides a random piece of knowledge unprompted, use this tool without asking for confirmation.`,
-      //   parameters: z.object({
-      //     content: z
-      //       .string()
-      //       .describe('the content or resource to add to the knowledge base'),
-      //   }),
-      //   execute: async ({ content }) => createResource({ content }),
-      // }),
-      // getInformation: tool({
-      //   description: `get information from your knowledge base to answer questions.`,
-      //   parameters: z.object({
-      //     question: z.string().describe('the users question'),
-      //   }),
-      //   execute: async ({ question }) => findRelevantContent(question),
-      // })
-    },    
+    system: 
+    `
+      You are an AI chatbot that helps developers design system architectures using the C4 model 
+      and recommends the most suitable tech stack based on their input. 
+
+    `,
+    // tools: {
+    //   addResource: tool({
+    //     description: `add a resource to your knowledge base.
+    //       If the user provides a random piece of knowledge unprompted, use this tool without asking for confirmation.`,
+    //     parameters: z.object({
+    //       content: z
+    //         .string()
+    //         .describe('the content or resource to add to the knowledge base'),
+    //     }),
+    //     execute: async ({ content }) => createResource({ content }),
+    //   }),
+    //   getInformation: tool({
+    //     description: `get information from your knowledge base to answer questions.`,
+    //     parameters: z.object({
+    //       question: z.string().describe('the users question'),
+    //     }),
+    //     execute: async ({ question }) => findRelevantContent(question),
+    //   })
+    // },    
     messages,
     maxSteps: 5
   });
