f4d53c8e553932552a9b6780a44bcad6da7b32c0
Leon Malisov <leonmalisov@gmail.com>
2025-02-25 03:15:50 -0500

    append container diagram object to chat

--- app/page.tsx
+++ app/page.tsx
@@ -10,18 +10,13 @@ import { GroupedContainerSchema } from '@/lib/schemas/groupedContainer';
 import { mockGroupedContainer } from './api/object/mockGroupedContainer';
 import { GroupedContainer, GroupedContainerProps } from '@/components/GroupedContainer';
 import { QuestionAnswers } from './types/QuestionAnswers';
-
+import { MemoizedMarkdown } from '@/components/memoized-markdown';
 export default function Page(
 ) {
   const [questionsAnswers, setQuestionsAnswers] = useState<string>('');
   // let aiObject = {
   //   object: mockGroupedContainer
   // }
-
-  let aiObject = useObject({
-    api: '/api/object',
-    schema: GroupedContainerSchema,
-  });
 
   let mockObject = mockGroupedContainer;
   const handleQuestionnaireSubmit = (answers: QuestionAnswers[]) => {
@@ -35,69 +30,86 @@ export default function Page(
     // maxSteps: 3,
   });
 
+  let aiObject = useObject({
+    api: '/api/object',
+    schema: GroupedContainerSchema,
+    onFinish: (object) => {
+      chat.append({
+        role: "system",
+        content: JSON.stringify(object),
+      });
+    }
+  });
+
   const { messages, input, handleInputChange, handleSubmit } = chat;
   return (
-    <div className="flex flex-col w-full py-24 mx-auto stretch">
-      <div className="space-y-4">
-        <Questionnaire onSubmit={handleQuestionnaireSubmit} />
+    <div className="flex flex-col items-center w-full py-24 mx-auto stretch">
+      <div className="space-y-4 w-full flex flex-col items-center">
+        <div className="w-1/2">
+          <Questionnaire onSubmit={handleQuestionnaireSubmit} />
+        </div>
+        
         {aiObject.object && aiObject?.object?.groups && (
-          <GroupedContainer {...aiObject.object as GroupedContainerProps} />
+          <div className="w-3/4">
+            <GroupedContainer {...aiObject.object as GroupedContainerProps} />
+          </div>
         )}
 
-        {messages.map(message => (
-          message.role != "system" ? (
-            <div key={message.id} className="max-w-2xl whitespace-pre-wrap">
-              <div>
-                <div className="font-bold">{message.role}</div>                
-                <div className="prose dark:prose-invert max-w-none">
-                  {message.content.length > 0 ? (
-                    // <MemoizedMarkdown content={message.content} id={message.id} />
-                    <p>{message.content}</p>
-                  ) : (
-                    <span className="italic font-light">
-                      {'calling tool: ' + message?.toolInvocations?.[0].toolName}
-                    </span>
-                  )}
-                </div >
-              </div>
+        <div className="w-1/2">
+          {messages.map(message => (
+            message.role != "system" ? (
+              <div key={message.id} className="whitespace-pre-wrap">
+                <div>
+                  <div className="font-bold">{message.role}</div>                
+                  <div className="">
+                    {message.content.length > 0 ? (
+                      <MemoizedMarkdown content={message.content} id={message.id} />
+                    ) : (
+                      <span className="italic font-light">
+                        {'calling tool: ' + message?.toolInvocations?.[0].toolName}
+                      </span>
+                    )}
+                  </div>
+                </div>
 
-              <div>
-                {message.toolInvocations?.map(toolInvocation => {
-                  const { toolName, toolCallId, state } = toolInvocation;
+                <div>
+                  {message.toolInvocations?.map(toolInvocation => {
+                    const { toolName, toolCallId, state } = toolInvocation;
 
-                  if (state === 'result') {
-                    if (toolName === 'displayWeather') {
-                      const { result } = toolInvocation;
+                    if (state === 'result') {
+                      if (toolName === 'displayWeather') {
+                        const { result } = toolInvocation;
+                        return (
+                          <div key={toolCallId}>
+                            <Weather {...result} />
+                          </div>
+                        );
+                      }
+                    } else {
                       return (
                         <div key={toolCallId}>
-                          <Weather {...result} />
+                          {toolName === 'displayWeather' ? (
+                            <div>Loading weather...</div>
+                          ) : null}
                         </div>
                       );
                     }
-                  } else {
-                    return (
-                      <div key={toolCallId}>
-                        {toolName === 'displayWeather' ? (
-                          <div>Loading weather...</div>
-                        ) : null}
-                      </div>
-                    );
-                  }
-                })}
+                  })}
+                </div>
               </div>
-            </div>
-          ) : null
-        ))}
+            ) : null
+          ))}
+        </div>
       </div>
 
-      <form onSubmit={handleSubmit}>
+      {/* <form onSubmit={handleSubmit}>
         <input
           className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
           value={input}
           placeholder="Say something..."
           onChange={handleInputChange}
         />
-      </form>
+      </form> */}
     </div>
   );
 }
